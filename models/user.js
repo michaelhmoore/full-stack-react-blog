18649d123ac8e47d88bd411716a55e3d6ec3ad6b
michael moore <michael.moore@hey.com>
2021-04-08 16:52:00 -0400

    added register, login, logout routes. add session middleware

--- models/user.js
+++ models/user.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.hasMany(models.Post)
-      User.hasMany(modles.Comment)
+      User.hasMany(models.Comment)
     }
   };
   User.init({
